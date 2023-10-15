44949ed463a8604af8f375e0355555dd3ec3e9cf
devlucasrbr@outlook.com <vinnytecnologia@gmail.com>
2023-10-15 01:19:21 -0300

    adicionando arquivos

--- code.js
+++ code.js
@@ -9,7 +9,32 @@ $(document).ready(function() {
 
     $('#telefone').mask('(00) 00000-0000');
     $('#cnpj').mask('00.000.000/0000-00');
+    $('#cep').mask('00.000-000')
     
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cnpj: {
+                required: true
+            },
+            sistema: {
+                required: true
+            },
+
+        },
+        messages: {
+            nome: 'Por Favor, Digite Seu Nome'
+            }
+    })
     
 })
 
